116bc937a7fb3a3a434fa59a2ca5d617d71b69cb
jefferson <jeffepok@gmail.com>
2021-03-15 09:38:16 +0000

    Declared all Bank methods in IBank interface

--- src/main/java/org/jeybank/interfaces/IBank.java
+++ src/main/java/org/jeybank/interfaces/IBank.java
@@ -1,3 +1,8 @@
 package org.jeybank.interfaces;
+
 public interface IBank {
+    boolean addBranch(String branchName);
+    boolean addCustomer(String branchName, String customerName, double initialTransaction);
+    boolean addCustomerTransaction(String branchName, String customerName, double transaction);
+    boolean listCustomers(String branchName, boolean printTransactions);
 }
